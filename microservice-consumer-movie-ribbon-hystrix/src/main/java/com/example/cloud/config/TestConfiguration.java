88e6ba999199eb8b3bcb7968d30ce48da102d411
joe zhang <joe.zhang@ringcentral.com>
2019-02-13 13:51:23 +0800

    introduce hystrix

---
+++ microservice-consumer-movie-ribbon-hystrix/src/main/java/com/example/cloud/config/TestConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.cloud.config;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@RibbonClient(name = "microservice-provide-user", configuration = RibbonConfiguration.class)
+public class TestConfiguration {
+}
